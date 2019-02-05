1c86f66df5ca21b110527b8549abfb32da1a6a1a
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2019-02-05 16:07:50 +0530

    Fixed 404

--- src/app/fourohfour/fourohfour.component.ts
+++ src/app/fourohfour/fourohfour.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { blackHeader } from '../header/header.component';
 import { Title } from '@angular/platform-browser';
+import { isMobile } from '../app.component';
 
 @Component({
   selector: 'app-fourohfour',
@@ -8,6 +9,8 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./fourohfour.component.css']
 })
 export class FourohfourComponent implements OnInit {
+
+  isMobile = isMobile;
 
   constructor(private titleService: Title) {
     this.titleService.setTitle('Incident 2019 - Error 404');
