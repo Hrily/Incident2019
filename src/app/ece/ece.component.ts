cec3e6485cb37d5f57f7a30e39b1e8d895af9235
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2019-02-20 18:15:16 +0530

    Update Internship content (#38)
    * Update verzeo.component.html
    
    * all the changes
    
    * Fixed mobile layout and typo

---
+++ src/app/ece/ece.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { whiteHeader } from '../header/header.component';
+
+@Component({
+  selector: 'app-ece',
+  templateUrl: './ece.component.html',
+  styleUrls: ['./ece.component.css']
+})
+export class EceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
